ff68b9895b15eaba815de93fc29667cc3ba38d37
Guillaume Bellut <guillaume.bellut@edf.fr>
2020-07-19 22:43:11 +0200

    Added connection callback

--- MentalDiceExample/DiceCollector.swift
+++ MentalDiceExample/DiceCollector.swift
@@ -10,15 +10,32 @@ import UIKit
 import MentalDiceFramework
 
 class DiceCollector: ObservableObject {
+    @Published var connected = false
     @Published var dice = MentalDice.shared.dice
 
     init() {
         let mentalDice = MentalDice.shared
         mentalDice.delegate = self
     }
+
+    func connect() {
+        MentalDice.shared.connect()
+    }
+
+    func disconnect() {
+        MentalDice.shared.disconnect()
+    }
 }
 
 extension DiceCollector: MentalDiceDelegate {
+    func didConnect() {
+        connected = true
+    }
+
+    func didDisconnect() {
+        connected = false
+    }
+
     func didUpdate(dice value: [Die]) {
         dice = value
     }
